d2b9c3ee34ace3e8758c95fe21e02aaca2570808
Herman Narkaytis <harkaytis@gmail.com>
2011-05-02 22:41:24 +0400

    Code cleanup

--- examples/emptystruct.c
+++ examples/emptystruct.c
@@ -12,10 +12,11 @@ main (int argc, char * argv[])
 {
   empty_t empty;
   
-  char* str = RL_SAVE_CINIT (empty_t, &empty);
+  char * str = RL_SAVE_CINIT (empty_t, &empty);
   memset (&empty, 0, sizeof (empty));
   RL_LOAD_CINIT (empty_t, str, &empty);
-  RL_FREE (str);
+  if (str)
+    RL_FREE (str);
   
   return (EXIT_SUCCESS);
 }
