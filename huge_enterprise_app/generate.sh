62321982f324e7a5492ae22d2986d9c15957ccf1
Herman Narkaytis <harkaytis@gmail.com>
2011-05-29 14:36:16 +0400

    replaced echo on cat

--- huge_enterprise_app/generate.sh
+++ huge_enterprise_app/generate.sh
@@ -5,75 +5,58 @@ numfiles=$1
 generate_header()
 {
     local fileno=$1
-    local name="header_${fileno}.h"
-    
-    : > ${name}
-    
-    (
-        echo
-        echo "#ifndef _METARESC_HUGE_ENTERPRISE_APP_TEST_HEADER_${fileno}_INCLUDED_"
-        echo "#define _METARESC_HUGE_ENTERPRISE_APP_TEST_HEADER_${fileno}_INCLUDED_"
-        echo
-        echo '#include <reslib.h>'
-        echo
-        echo "TYPEDEF_STRUCT (struct_${fileno}_t,"
-	echo '    (int, a),'
-        echo '    (long, b),'
-        echo '    (void*, c),'
-        echo '    )'
-        echo
-        echo "void f${fileno}(void);"
-        echo
-        echo '#endif // guardian'
-    ) >> ${name}
+    cat > header_${fileno}.h <<EOF
+#ifndef _METARESC_HUGE_ENTERPRISE_APP_TEST_HEADER_${fileno}_INCLUDED_
+#define _METARESC_HUGE_ENTERPRISE_APP_TEST_HEADER_${fileno}_INCLUDED_
+#include <reslib.h>
+
+TYPEDEF_STRUCT (struct_${fileno}_t,
+		(int, a),
+		(long, b),
+		(void*, c),
+		)
+void f${fileno}(void);
+#endif // guardian
+EOF
 }
 
 generate_source()
 {
     local fileno=$1
-    local name="file_${fileno}.c"
-    
-    : > ${name}
-    
-    (
-        echo
-        echo "#include \"header_${fileno}.h\""
-        echo '#include "main.h"'
-        echo
-        echo "void f${fileno}(void)"
-        echo '{'
-        echo "  struct_${fileno}_t s;"
-        echo '  s.a = 3;'
-        echo '}'
-        echo
-    ) >> ${name}
+    cat > file_${fileno}.c <<EOF
+#include "header_${fileno}.h"
+#include "main.h"
+        
+void f${fileno}(void)
+{
+  struct_${fileno}_t s;
+  s.a = 0;
+}
+EOF
 }
 
 generate_main()
 {
-    (
-        echo
-        echo '#include <stdio.h>'
-        echo '#include "main.h"'
-        echo
-        echo
-        echo 'int main()'
-        echo '{'
-        echo '  puts("It has started!");'
-        echo '  return (0);'
-        echo '}'
-        echo
-    ) > main.c
+    cat > main.c <<EOF
+#include <stdio.h>
+#include "main.h"
+        
+        
+int main()
+{
+  puts("It has started!");
+  return (0);
+}
+EOF
 }
 
 generate_prefix_of_main_h()
 {
-    (
-        echo
-        echo "#ifndef _METARESC_HUGE_ENTERPRISE_APP_TEST_HEADER_MAIN_INCLUDED_"
-        echo "#define _METARESC_HUGE_ENTERPRISE_APP_TEST_HEADER_MAIN_INCLUDED_"
-        echo
-    ) > main.h
+    cat > main.h <<EOF
+#ifndef _METARESC_HUGE_ENTERPRISE_APP_TEST_HEADER_MAIN_INCLUDED_
+#define _METARESC_HUGE_ENTERPRISE_APP_TEST_HEADER_MAIN_INCLUDED_
+
+EOF
 }
 
 generate_include_for_main_h()
@@ -85,10 +68,10 @@ generate_include_for_main_h()
 
 generate_suffix_of_main_h()
 {
-    (
-        echo
-        echo '#endif'
-    ) >> main.h
+    cat >> main.h <<EOF
+
+#endif
+EOF
 }
 
 generate_prefix_of_main_h
