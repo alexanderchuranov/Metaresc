ca6c75feb74e45b90cd35616e93443cc87421f56
alexanderchuranov <alexanderchuranov+github@gmail.com>
2011-08-05 08:16:58 +0400

    DOC: XML and JSON formats are described.

---
+++ examples/deserializing_xml.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <reslib.h>
+
+TYPEDEF_STRUCT (employee_t,
+		(char *, firstname),
+		(char *, lastname),
+		(int, salary),
+		)
+
+int
+main (int argc, char * argv[])
+{
+  char const xml[] =
+    "<?xml version='1.0'?>"
+    "<employee>"
+      "<firstname>John</firstname>"
+      "<lastname>Doe</lastname>"
+      "<salary>123456</salary>"
+    "</employee>";
+  
+  employee_t e = RL_LOAD_XML (employee_t, xml);
+  
+  printf(
+    "First name: %s\nLast name: %s\nSalary: %u\n",
+    e.firstname, e.lastname, e.salary);
+  
+  return EXIT_SUCCESS;
+}
---
+++ examples/load_json.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <reslib.h>
+
+TYPEDEF_STRUCT (employee_t,
+		(char *, firstname),
+		(char *, lastname),
+		(int, salary),
+		)
+
+void print_info(employee_t const* e)
+{
+  printf(
+    "First name: %s\nLast name: %s\nSalary: %u\n",
+    e->firstname, e->lastname, e->salary);
+}
+
+int main (int argc, char * argv[])
+{
+  char json[] =
+    "{"                       "\n"
+    "  firstname : \"John\"," "\n"
+    "  lastname : \"Doe\","   "\n"
+    "  salary : 123456,"      "\n"
+    "}"                       "\n";
+  
+  employee_t e = RL_LOAD_JSON (employee_t, json);
+  
+  print_info(&e);
+  
+  char corrupted[] = "this is not a json text";
+  
+  if (RL_LOAD_JSON (employee_t, corrupted, &e))
+  {
+    print_info(&e);
+  }
+  else {
+    fprintf(stderr, "JSON parsing error!");
+    return EXIT_FAILURE;
+  }
+  
+  return EXIT_SUCCESS;
+}
