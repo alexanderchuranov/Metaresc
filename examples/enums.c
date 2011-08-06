84a73235458d27b17f50a1f3f3991773383fa4f8
Herman Narkaytis <harkaytis@gmail.com>
2011-08-06 14:58:15 +0400

    Added more samples on enums declaration

--- examples/enums.c
+++ examples/enums.c
@@ -4,7 +4,8 @@
 
 int main()
 {
-  printf("BLACK: %i\nRED: %i\nGREEN: %i\nYELLOW: %i\n",
-         BLACK, RED, GREEN, YELLOW);
+  printf("color_t enum desciptor\n%s\n", RL_SAVE_CINIT (rl_td_t, &RL_DESCRIPTOR_PREFIX (color_t)));
+  printf("BLACK: %i\nRED: %i\nGREEN: %i\nYELLOW: %i\nBLUE: %i\nMAGENTA: %i\nCYAN: %i\nWHITE: %i\n",
+         BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE);
   return 0;
 }
--- examples/enums.h
+++ examples/enums.h
@@ -2,12 +2,12 @@
 #include <reslib.h>
 
 TYPEDEF_ENUM (color_t,
-              (BLACK),        // becomes 0
-              (RED,     = 1),
-              (GREEN,      ), // becomes 2
-              (YELLOW),       // becomes 3
-              (BLUE,    = 4),
-              (MAGENTA, = 5),
-              (CYAN,    = 6),
-              (WHITE,   = 7)
+              BLACK,          // auto-enumerated and becomes 0
+              (RED),          // auto-enumerated and becomes 1
+              (GREEN,   = 2), // explicitly set to 2
+              (YELLOW,  = 3, "set to 3 - this is entry's comment"),
+              (BLUE,    = 4, "set to 4", { "meta data as a void pointer" }),
+              (MAGENTA, /* could be empty */, "becomes 5", { "next argument is a type of this poiner" }, "rl_char_array_t"),
+              (CYAN, /* auto-enumerated */, /* no comment */, { (color_t[]){ 6 } }, "color_t"),
+              WHITE /* trailing comma is optional */
               )
