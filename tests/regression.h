41a87b1e68a324dcece1de1e0b2afd6a36d386c8
Herman Narkaytis <harkaytis@gmail.com>
2012-01-05 22:32:48 +0300

    Simplified CMP macro

--- tests/regression.h
+++ tests/regression.h
@@ -6,8 +6,8 @@
 
 #include <reslib.h>
 
-#define TEST_METHODS XDR, SCM, CINIT, JSON, XML1, XML2
-//#define TEST_METHODS SCM
+//#define TEST_METHODS XDR, SCM, CINIT, JSON, XML1, XML2
+#define TEST_METHODS XDR
 
 extern Suite * suite;
 
@@ -29,20 +29,24 @@ extern Suite * suite;
   };									\
   START_TEST(NAME)
 
+#define MEMCMP(X, Y) memcmp (X, Y, sizeof (*(Y)))
+
 #define ASSERT_SAVE_LOAD(METHOD, TYPE, X, ...)				\
   RL_IF_ELSE (RL_IS_EMPTY (__VA_ARGS__))				\
-  (ASSERT_SAVE_LOAD_ (METHOD, TYPE, X, memcmp))				\
+  (ASSERT_SAVE_LOAD_ (METHOD, TYPE, X, MEMCMP))				\
   (ASSERT_SAVE_LOAD_ (METHOD, TYPE, X, __VA_ARGS__))
 
 #define ASSERT_SAVE_LOAD_(METHOD, TYPE, X, CMP) ({			\
       rl_rarray_t serialized = RL_SAVE_ ## METHOD ## _RA (TYPE, X);	\
-      TYPE restored = RL_LOAD_ ## METHOD ## _RA (TYPE, &serialized);	\
+      TYPE restored;							\
+      if (0 == RL_LOAD_ ## METHOD ## _RA (TYPE, &serialized, &restored)) \
+	ck_abort_msg ("load for method " #METHOD " on type " #TYPE "failed"); \
       if (0) printf ("%s\n", (char*)serialized.data);			\
       if (serialized.data)						\
 	RL_FREE (serialized.data);					\
-      ck_assert_msg ((0 == CMP (X, &restored, sizeof (TYPE))),		\
+      ck_assert_msg ((0 == CMP (X, &restored)),				\
 		     "restored value mismatched original for method " #METHOD " on type " #TYPE); \
-      restored;								\
+      RL_FREE_RECURSIVELY (TYPE, &restored);				\
     })
 
 #define ASSERT_SAVE_LOAD_TYPE(METHOD, TYPE, VALUE, ...) ({		\
@@ -57,10 +61,7 @@ extern Suite * suite;
 #define SERIAL(NAME, I, REC, X) REC; X
 #define ALL_METHODS(...) RL_FOR ((__VA_ARGS__), RL_NARG (TEST_METHODS), SERIAL, ASSERT_ITERATOR, TEST_METHODS)
 
-#define FDLD_CMP(X, ...) FDLD_CMP_ (X, __VA_ARGS__)
-#define FDLD_CMP_(X, Y, ...) (*(X) != *(Y))
-//#define FDLD_CMP_(X, Y, ...) ({printf ("x = %.20Lg y = %.20Lg diff = %.20Lg\n", (long double)*(X), (long double)*(Y), (long double)(*(X) - *(Y))); (*(X) != *(Y));})
-#define STRUCT_FDLD_CMP(X, ...) STRUCT_FDLD_CMP_ (X, __VA_ARGS__)
-#define STRUCT_FDLD_CMP_(X, Y, ...) ((X)->x != (Y)->x)
+#define FDLD_CMP(X, ...) (*(X) != *(__VA_ARGS__))
+#define STRUCT_FDLD_CMP(X, ...) ((X)->x != (__VA_ARGS__)->x)
 
 #endif /* _REGRESSION_H_ */
