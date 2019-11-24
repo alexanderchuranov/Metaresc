9f124dd14c14284a107453bba42e7fff784beea8
Herman Narkaytis <hnarkaytis@gmail.com>
2019-11-24 10:51:30 +0400

    Fix nan classification for floats

--- tests/flt_values.h
+++ tests/flt_values.h
@@ -72,15 +72,15 @@ TYPEDEF_STRUCT (ieee_754_float_nan_t,
 		BITFIELD (unsigned int, exponent, :8),
 		BITFIELD (sign_t, sign, :1))
 
-TYPEDEF_ENUM (ieee_754_float_nan_enum_t, (IEEE_754_FLOAT_NAN_ENUM_T, = (1 << 9) - 1, "ieee_754_float_nan"))
+TYPEDEF_ENUM (ieee_754_float_nan_enum_t, (IEEE_754_FLOAT_NAN_ENUM_T, = (1 << 8) - 1, "ieee_754_float_nan"))
 TYPEDEF_UNION (ieee_float_t,
 	       ANON_UNION (),
 	       (ieee_754_float_t, ieee_754_float),
 	       (ieee_754_float_nan_t, ieee_754_float_nan),
 	       float _float,
 	       uint32_t uint32,
-	       END_ANON_UNION ("quiet_nan"),
-	       BITFIELD (ieee_754_float_nan_enum_t, quiet_nan, :9, , .offset = 22 / 8, .param = { .bitfield_param = { .width = 9, .shift = 22 % 8, .size = 0, }, } ),
+	       END_ANON_UNION ("is_nan"),
+	       BITFIELD (ieee_754_float_nan_enum_t, is_nan, :8, , .offset = 23 / __CHAR_BIT__, .param = { .bitfield_param = { .width = 8, .shift = 23 % __CHAR_BIT__, .size = 0, }, } ),
 	       )
 
 TYPEDEF_STRUCT (ieee_754_double_t,
@@ -96,15 +96,15 @@ TYPEDEF_STRUCT (ieee_754_double_nan_t,
 		BITFIELD (unsigned int, exponent, :11),
 		BITFIELD (sign_t, sign, :1))
 
-TYPEDEF_ENUM (ieee_754_double_nan_enum_t, (IEEE_754_DOUBLE_NAN_ENUM_T, = (1 << 12) - 1, "ieee_754_double_nan"))
+TYPEDEF_ENUM (ieee_754_double_nan_enum_t, (IEEE_754_DOUBLE_NAN_ENUM_T, = (1 << 11) - 1, "ieee_754_double_nan"))
 TYPEDEF_UNION (ieee_double_t,
 	       ANON_UNION (),
 	       (ieee_754_double_t, ieee_754_double),
 	       (ieee_754_double_nan_t, ieee_754_double_nan),
 	       double _double,
 	       uint64_t uint64,
-	       END_ANON_UNION ("quiet_nan"),
-	       BITFIELD (ieee_754_double_nan_enum_t, quiet_nan, :12, , .offset = (32 + 19) / 8, .param = { .bitfield_param = { .width = 12, .shift = (32 + 19) % 8, .size = 0, }, } ),
+	       END_ANON_UNION ("is_nan"),
+	       BITFIELD (ieee_754_double_nan_enum_t, is_nan, :11, , .offset = (32 + 19) / 8, .param = { .bitfield_param = { .width = 11, .shift = (32 + 19) % 8, .size = 0, }, } ),
 	       )
 
 TYPEDEF_STRUCT (ieee_854_long_double_t,
@@ -124,8 +124,8 @@ TYPEDEF_STRUCT (ieee_854_long_double_nan_t,
 		VOID (unsigned int, empty, :16))
 
 TYPEDEF_ENUM (ieee_854_long_double_nan_enum_t,
-	      (IEEE_854_LONG_DOUBLE_NAN_ENUM_T, = (1 << 17) - 1, "ieee_854_long_double_nan"),
-	      (IEEE_854_LONG_DOUBLE_NAN_ZERO_ONE_ENUM_T, = ((1 << 17) - 1) ^ (1 << 1), "ieee_854_long_double_nan"),
+	      (IEEE_854_LONG_DOUBLE_NAN_ENUM_T, = (1 << 15) - 1, "ieee_854_long_double_nan"),
+	      (IEEE_854_LONG_DOUBLE_NAN_ZERO_ONE_ENUM_T, = ((1 << 15) - 1) ^ (1 << 1), "ieee_854_long_double_nan"),
 	      )
 
 TYPEDEF_UNION (ieee_long_double_t,
@@ -133,7 +133,7 @@ TYPEDEF_UNION (ieee_long_double_t,
 	       (ieee_854_long_double_t, ieee_854_long_double),
 	       (ieee_854_long_double_nan_t, ieee_854_long_double_nan),
 	       long double long_double,
-	       END_ANON_UNION ("quiet_nan"),
-	       BITFIELD (ieee_854_long_double_nan_enum_t, quiet_nan, :17, , .offset = (32 + 30) / 8, .param = { .bitfield_param = { .width = 17, .shift = (32 + 30) % 8, .size = 0, }, } ),
+	       END_ANON_UNION ("is_nan"),
+	       BITFIELD (ieee_854_long_double_nan_enum_t, is_nan, :15, , .offset = (32 + 30) / 8, .param = { .bitfield_param = { .width = 15, .shift = (32 + 30) % 8, .size = 0, }, } ),
 	       )
 #endif /* _FLT_VALUES_H_ */
