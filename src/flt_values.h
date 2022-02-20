64b7e3a19c0424c0d9429f3a9c0e817827d5af17
Herman Narkaytis <hnarkaytis@hnarkaytis-mbp.lan>
2022-02-20 23:20:55 +0400

    Support M1 long double

--- src/flt_values.h
+++ src/flt_values.h
@@ -11,6 +11,14 @@
 #include <values.h>
 #endif /* HAVE_VALUES_H */
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif /* M_PI */
+
+#ifndef M_E
+#define M_E 2.7182818284590452354
+#endif /* M_E */
+
 #ifndef FLT_MAX
 #define FLT_MAX (0x1.fffffep+127)
 #endif /* FLT_MAX */
@@ -35,6 +43,36 @@
 #define DBL_EPSILON (0x1p-52)
 #endif /* DBL_EPSILON */
 
+#ifdef MR_LONG_DOUBLE_IS_DOUBLE
+
+#ifndef LDBL_MAX
+#define LDBL_MAX DBL_MAX
+#endif /* LDBL_MAX */
+
+#ifndef LDBL_MIN
+#define LDBL_MIN DBL_MIN
+#endif /* LDBL_MIN */
+
+#ifndef LDBL_EPSILON
+#define LDBL_EPSILON DBL_EPSILON
+#endif /* LDBL_EPSILON */
+
+#define LD_NAN NAN
+#define LD_LDBL_MAX DBL_MAX
+#define LD_LDBL_MIN DBL_MIN
+
+#define MR_SIZEOF_LONG_DOUBLE sizeof (long double)
+
+#define MR_ISNAN(X)							\
+  __builtin_choose_expr (__builtin_types_compatible_p (__typeof__ (X), float), \
+			 ((ieee_float_t)(float)(X)).ieee_754_float_nan.exponent == IEEE_754_FLOAT_NAN_ENUM_T, \
+			 __builtin_choose_expr (__builtin_types_compatible_p (__typeof__ (X), double), \
+						((ieee_double_t)(double)(X)).ieee_754_double_nan.exponent == IEEE_754_DOUBLE_NAN_ENUM_T, \
+						__builtin_choose_expr (__builtin_types_compatible_p (__typeof__ (X), long double), \
+								       ((ieee_double_t)(double)(X)).ieee_754_double_nan.exponent == IEEE_754_DOUBLE_NAN_ENUM_T, \
+								       false)))
+#else /* ! MR_LONG_DOUBLE_IS_DOUBLE */
+
 #ifndef LDBL_MAX
 #define LDBL_MAX (0x1.ffffffffffffffffp+16383L)
 #endif /* LDBL_MAX */
@@ -47,19 +85,22 @@
 #define LDBL_EPSILON (0x1p-64)
 #endif /* LDBL_EPSILON */
 
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif /* M_PI */
-
-#ifndef M_E
-#define M_E 2.7182818284590452354
-#endif /* M_E */
-
 #define LD_NAN ((ieee_long_double_t){ { .ieee_854_long_double_nan = { .quiet_nan = !0, .one = 1, .exponent = -1, }, }, }).long_double
 #define LD_LDBL_MAX ((ieee_long_double_t){ { .ieee_854_long_double = { .mantissa = -1, .exponent = -2, .sign = PLUS, }, }, }).long_double
 #define LD_LDBL_MIN ((ieee_long_double_t){ { .ieee_854_long_double = { .mantissa = (1UL << (IEEE_854_LONG_DOUBLE_MANTISSA - 1)), .exponent = 1, .sign = PLUS, }, }, }).long_double
 
-#define MR_SIZEOF_LONG_DOUBLE (sizeof (ieee_854_long_double_t))
+#define MR_SIZEOF_LONG_DOUBLE sizeof (ieee_854_long_double_t)
+
+#define MR_ISNAN(X)							\
+  __builtin_choose_expr (__builtin_types_compatible_p (__typeof__ (X), float), \
+			 ((ieee_float_t)(float)(X)).ieee_754_float_nan.exponent == IEEE_754_FLOAT_NAN_ENUM_T, \
+			 __builtin_choose_expr (__builtin_types_compatible_p (__typeof__ (X), double), \
+						((ieee_double_t)(double)(X)).ieee_754_double_nan.exponent == IEEE_754_DOUBLE_NAN_ENUM_T, \
+						__builtin_choose_expr (__builtin_types_compatible_p (__typeof__ (X), long double), \
+								       ((((ieee_long_double_t)(long double)(X)).ieee_854_long_double_nan.exponent == IEEE_854_LONG_DOUBLE_NAN_ENUM_T) || \
+									(((ieee_long_double_t)(long double)(X)).ieee_854_long_double_nan.exponent == IEEE_854_LONG_DOUBLE_NAN_ZERO_ONE_ENUM_T)), \
+								       false)))
+#endif /* MR_LONG_DOUBLE_IS_DOUBLE */
 
 #define MR_CLD_PACK(CLD) ({				\
       complex_long_double_t __cld__ = (CLD);		\
@@ -85,13 +126,4 @@
       __cld__;						\
     })
   
-#define MR_ISNAN(X)							\
-  __builtin_choose_expr (__builtin_types_compatible_p (__typeof__ (X), float), \
-			 ((ieee_float_t)(float)(X)).ieee_754_float_nan.exponent == IEEE_754_FLOAT_NAN_ENUM_T, \
-			 __builtin_choose_expr (__builtin_types_compatible_p (__typeof__ (X), double), \
-						((ieee_double_t)(double)(X)).ieee_754_double_nan.exponent == IEEE_754_DOUBLE_NAN_ENUM_T, \
-						__builtin_choose_expr (__builtin_types_compatible_p (__typeof__ (X), long double), \
-								       ((((ieee_long_double_t)(long double)(X)).ieee_854_long_double_nan.exponent == IEEE_854_LONG_DOUBLE_NAN_ENUM_T) || \
-									(((ieee_long_double_t)(long double)(X)).ieee_854_long_double_nan.exponent == IEEE_854_LONG_DOUBLE_NAN_ZERO_ONE_ENUM_T)), \
-								       false)))
 #endif /* _FLT_VALUES_H_ */
