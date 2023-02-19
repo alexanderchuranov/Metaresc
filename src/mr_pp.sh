af86f8c256c2107dbdac0800a8db470d0edf080c
Herman Narkaytis <hnarkaytis@gmail.com>
2023-02-19 03:23:02 +0100

    Code clean up

--- src/mr_pp.sh
+++ src/mr_pp.sh
@@ -16,7 +16,7 @@ while [ $I -lt $PP_DEPTH ]
 do
   NEXT=$(( $I + 1 ))
   echo "#define MR_FOREACH$NEXT(X, _0, ...) X (_0) MR_FOREACH$I (X, __VA_ARGS__)" 
-  I=$(( $I + 1 ))
+  I=$NEXT
 done
 echo 
 
@@ -27,38 +27,38 @@ while [ $I -lt $PP_DEPTH ]
 do
   NEXT=$(( $I + 1 ))
   echo "#define MR_FOR$NEXT(NAME, OP, FUNC, X, ...) OP (NAME, $I, FUNC (NAME, X, $NEXT), MR_FOR$I (NAME, OP, FUNC, __VA_ARGS__))" 
-  I=$(( $I + 1 ))
+  I=$NEXT
 done
 echo 
 
-/bin/echo -n "#define MR_ARG"$PP_DEPTH"(" 
+printf "#define MR_ARG$PP_DEPTH(" 
 I=1
 while [ $I -le $PP_DEPTH ]
 do
-  /bin/echo -n "_$I, "
-  I=$(( $I + 1 ))
+  printf "_$I, "
+  ((I++))
 done
-echo "...) _"$PP_DEPTH 
+echo "...) _$PP_DEPTH"
 echo 
 
 echo "#define MR_NARG(...) MR_NARG_ (0, ##__VA_ARGS__)" 
 
-/bin/echo -n "#define MR_NARG_(...) MR_ARG$PP_DEPTH (__VA_ARGS__, " 
+printf "#define MR_NARG_(...) MR_ARG$PP_DEPTH (__VA_ARGS__, " 
 I=$(( $PP_DEPTH - 2 ))
 while [ $I -ge 0 ]
 do
-  /bin/echo -n "$I, "
-  I=$(( $I - 1 ))
+  printf "$I, "
+  ((I--))
 done
 echo ")" 
 echo 
 
-/bin/echo -n "#define MR_HAS_COMMA(...) MR_ARG"$PP_DEPTH "(__VA_ARGS__, " 
+printf "#define MR_HAS_COMMA(...) MR_ARG$PP_DEPTH (__VA_ARGS__, " 
 I=2
 while [ $I -lt $PP_DEPTH ]
 do
-  /bin/echo -n "1, "
-  I=$(( $I + 1 ))
+  printf "1, "
+  ((I++))
 done
 echo "0)" 
 echo 
