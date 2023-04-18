9d473c03a6bd5ad1a32781f36e404d118bd664da
Ivan Korolev <sociopart@hotmail.com>
2023-04-18 18:15:56 +0400

    WIP yaml_save / yaml_load impl

---
+++ src/yaml1_load.y
@@ -0,0 +1,154 @@
+/* -*- C -*- */
+/* I hate this bloody country. Smash. */
+/* This file is a part of Metaresc project */
+
+%code top {
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MR_YAML_DEBUG 0
+/* Pass the argument to yyparse through to yylex. */
+#define MR_YAML_LTYPE mr_token_lloc_t
+#define MR_LOAD (mr_yaml_get_extra (scanner))
+#define mr_yaml_error MR_PARSE_ERROR
+
+#include <metaresc.h>
+#include <mr_stringify.h>
+#include <lexer.h>
+#include <mr_value.h>
+#include <yaml1_load.tab.h>
+#define YYSTYPE MR_YAML_STYPE
+#define YYLTYPE MR_YAML_LTYPE
+#include <yaml1_load.lex.h>
+
+}
+
+%code {
+void
+yaml_unquote_str (mr_substr_t * substr, char * dst)
+{
+  int i, size, length;
+
+  static bool initialized = false;
+  static char map[MR_ESC_CHAR_MAP_SIZE];
+
+  if (!initialized)
+    {
+      memset (map, 0, sizeof (map));
+      for (i = 0; i < MR_ESC_CHAR_MAP_SIZE; ++i)
+	if (mr_esc_char_map[i])
+	  map[(unsigned char)mr_esc_char_map[i]] = i;
+      initialized = true;
+    }
+
+  if (NULL == substr->str)
+    return;
+
+  length = 0;
+  for (i = 0; i < substr->length; ++i)
+    {
+      if ('\\' == substr->str[i])
+	{
+	  int c = map[(unsigned char)substr->str[++i]];
+	  if (c > 0)
+	    dst[length++] = c;
+	  else if (1 == sscanf (&substr->str[i], "x%02x%n", &c, &size))
+	    {
+	      i += size - 1;
+	      dst[length++] = c;
+	    }
+      else if (1 == sscanf (&substr->str[i], "u%04x%n", &c, &size))
+	    {
+	      i += size - 1;
+	      dst[length++] = c;
+	    }
+	  else
+	    dst[length++] = substr->str[i];
+	}
+      else
+	dst[length++] = substr->str[i];
+    }
+  dst[length] = 0;
+}
+
+}
+
+%define api.prefix {mr_yaml_}
+%define api.pure full
+%param {void * scanner}
+%locations
+ /* generate include-file with symbols and types */
+%defines
+
+ /* a more advanced semantic type */
+%union {
+  mr_value_t value;
+  mr_substr_t string;
+}
+
+/* Bison declarations. */
+%token <value> TOK_YAML_NUMBER
+%token <string> TOK_YAML_STRING
+%token TOK_YAML_SEMICOLON TOK_YAML_LBRACE TOK_YAML_RBRACE TOK_YAML_LBRACKET TOK_YAML_RBRACKET TOK_YAML_COMMA TOK_YAML_NULL TOK_YAML_ERROR
+
+%start yaml
+
+%% /* The grammar follows. */
+
+yaml: element
+
+element: push_node value pop_node
+
+pop_node: {
+  mr_load_t * mr_load = MR_LOAD;
+  mr_load->parent = mr_load->ptrs->ra[mr_load->parent].parent;
+}
+
+push_node: {
+  mr_load_t * mr_load = MR_LOAD;
+  mr_load->parent = mr_parse_add_node (mr_load);
+  if (mr_load->parent < 0)
+  {
+    YYERROR;
+  }
+  mr_load->ptrs->ra[mr_load->parent].idx = mr_load->parent;
+}
+
+value: object | array
+| TOK_YAML_STRING {
+  mr_load_t * mr_load = MR_LOAD;
+  mr_load->ptrs->ra[mr_load->parent].load_params.mr_value.value_type = MR_VT_QUOTED_SUBSTR;
+  mr_load->ptrs->ra[mr_load->parent].load_params.mr_value.vt_quoted_substr.substr.str = &mr_load->str[$1.str - mr_load->buf];
+  mr_load->ptrs->ra[mr_load->parent].load_params.mr_value.vt_quoted_substr.substr.length = $1.length;
+  mr_load->ptrs->ra[mr_load->parent].load_params.mr_value.vt_quoted_substr.unquote = yaml_unquote_str;
+  }
+| TOK_YAML_NUMBER {
+  mr_load_t * mr_load = MR_LOAD;
+  mr_load->ptrs->ra[mr_load->parent].load_params.mr_value = $1;
+  }
+| TOK_YAML_NULL {
+  mr_load_t * mr_load = MR_LOAD;
+  mr_load->ptrs->ra[mr_load->parent].flags.is_null = true;
+  mr_load->ptrs->ra[mr_load->parent].load_params.mr_value.value_type = MR_VT_INT;
+  mr_load->ptrs->ra[mr_load->parent].load_params.mr_value.vt_int = 0;
+  }
+
+object: TOK_YAML_LBRACE TOK_YAML_RBRACE
+| TOK_YAML_LBRACE members TOK_YAML_RBRACE
+
+members: member | member TOK_YAML_COMMA members
+
+member: TOK_YAML_STRING TOK_YAML_COLON element {
+  mr_load_t * mr_load = MR_LOAD;
+  int idx = mr_load->ptrs->ra[mr_load->parent].last_child;
+  mr_load->ptrs->ra[idx].name = mr_get_static_field_name_from_substring (&$1);
+}
+
+array: TOK_YAML_LBRACKET TOK_YAML_RBRACKET
+| TOK_YAML_LBRACKET elements TOK_YAML_RBRACKET
+
+elements: element | element TOK_YAML_COMMA elements
+
+%%
+
+MR_LOAD_FUNC (yaml);
---
+++ src/yaml_save.c
@@ -0,0 +1,304 @@
+/* -*- C -*- */
+/* I hate this bloody country. Smash. */
+/* This file is a part of Metaresc project */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <yaml.h>
+
+#include <metaresc.h>
+#include <mr_save.h>
+#include <mr_stringify.h>
+
+#define COMPLEX_REAL_IMAG_DELIMITER " + "
+#define YAML_INDENT_SPACES (2)
+#define YAML_INDENT_TEMPLATE "%*s"
+#define YAML_NULL "~"
+
+#define YAML_QUOTE_CHAR_PATTERN "\\u%04x"
+#define YAML_BITMASK_DELIMITER " | "
+
+static int
+yaml_printf_char (mr_rarray_t * mr_ra_str, mr_ptrdes_t * ptrdes)
+{
+  unsigned char c = *(unsigned char*)ptrdes->data.ptr;
+  char mapped = mr_esc_char_map[c];
+  int count = 0;
+
+  count += TRY_CATCH_THROW (mr_ra_append_char (mr_ra_str, '"'));
+  if (c == '"')
+    mapped = c;
+  if (mapped)
+    {
+      count += TRY_CATCH_THROW (mr_ra_append_char (mr_ra_str, '\\'));
+      count += TRY_CATCH_THROW (mr_ra_append_char (mr_ra_str, mapped));
+    }
+  else if (isprint (c))
+    count += TRY_CATCH_THROW (mr_ra_append_char (mr_ra_str, c));
+  else
+    count += TRY_CATCH_THROW (mr_ra_printf (mr_ra_str, YAML_QUOTE_CHAR_PATTERN, c));
+  count += TRY_CATCH_THROW (mr_ra_append_char (mr_ra_str, '"'));
+  return (count);
+}
+
+static int
+yaml_printf_complex_float (mr_rarray_t * mr_ra_str, mr_ptrdes_t * ptrdes)
+{
+  int count = 0;
+
+  count += TRY_CATCH_THROW (mr_ra_printf_complex_float (mr_ra_str, ptrdes, COMPLEX_REAL_IMAG_DELIMITER));
+  
+  return (count);
+}
+
+static int
+yaml_printf_complex_double (mr_rarray_t * mr_ra_str, mr_ptrdes_t * ptrdes)
+{
+  int count = 0;
+
+  count += TRY_CATCH_THROW (mr_ra_printf_complex_double (mr_ra_str, ptrdes, COMPLEX_REAL_IMAG_DELIMITER));
+  
+  return (count);
+}
+
+static int
+yaml_printf_complex_long_double_t (mr_rarray_t * mr_ra_str, mr_ptrdes_t * ptrdes)
+{
+  int count = 0;
+
+  count += TRY_CATCH_THROW (mr_ra_printf_complex_long_double_t (mr_ra_str, ptrdes, COMPLEX_REAL_IMAG_DELIMITER));
+  
+  return (count);
+}
+
+static int
+yaml_printf_char_array (mr_rarray_t * mr_ra_str, mr_ptrdes_t * ptrdes)
+{
+  typeof (ptrdes->fdp->size) size = ptrdes->non_persistent ? (ptrdes->tdp ? ptrdes->tdp->size : 0) : ptrdes->fdp->size;
+  char buffer[size + 1];
+  strncpy (buffer, ptrdes->data.ptr, size);
+  buffer[size] = 0;
+  return (mr_ra_printf_quote_string (mr_ra_str, buffer, YAML_QUOTE_CHAR_PATTERN));
+}
+
+static int
+yaml_printf_string (mr_rarray_t * mr_ra_str, mr_ptrdes_t * ptrdes)
+{
+  return (mr_ra_printf_quote_string (mr_ra_str, *(char**)ptrdes->data.ptr, YAML_QUOTE_CHAR_PATTERN));
+}
+
+static int
+yaml_printf_bitmask (mr_rarray_t * mr_ra_str, mr_ptrdes_t * ptrdes)
+{
+  int count = 0;
+
+  count += TRY_CATCH_THROW (mr_ra_printf_bitmask (mr_ra_str, ptrdes, YAML_BITMASK_DELIMITER));
+  
+  return (count);
+}
+
+static int
+yaml_printf_bitfield (mr_rarray_t * mr_ra_str, mr_ptrdes_t * ptrdes)
+{
+  if (MR_TYPE_ENUM == ptrdes->mr_type_aux)
+    {
+      mr_ptrdes_t _ptrdes = *ptrdes;
+      uint64_t value;
+
+      mr_save_bitfield_value (ptrdes, &value);
+      _ptrdes.data.ptr = &value;
+      return (yaml_printf_bitmask (mr_ra_str, &_ptrdes));
+    }
+
+  return (mr_ra_printf_bitfield (mr_ra_str, ptrdes, YAML_BITMASK_DELIMITER));
+}
+
+static int
+yaml_printf_struct (mr_rarray_t * mr_ra_str, mr_ptrdes_t * ptrdes)
+{
+  ptrdes->res.data.string = "}";
+  ptrdes->res.type = "string";
+  ptrdes->res.MR_SIZE = 0;
+  return (mr_ra_append_string (mr_ra_str, "\n"));
+}
+
+static int
+yaml_printf_func (mr_rarray_t * mr_ra_str, mr_ptrdes_t * ptrdes)
+{
+  const char * func_str = mr_serialize_func (*(void**)ptrdes->data.ptr);
+
+  if (func_str) /* pointer serialized as name */
+    return (mr_ra_printf (mr_ra_str, "!!str \"%s\"", func_str));
+  else
+    return (mr_ra_printf (mr_ra_str, "!!str \"0x%llx\"", (unsigned long long int)*(uintptr_t*)ptrdes->data.ptr));
+}
+
+static int
+yaml_printf_array (mr_rarray_t * mr_ra_str, mr_ptrdes_t * ptrdes)
+{
+  ptrdes->res.data.string = "]";
+  ptrdes->res.type = "string";
+  ptrdes->res.MR_SIZE = 0;
+  return (mr_ra_append_string (mr_ra_str, "-\n"));
+}
+
+static int
+yaml_printf_void (mr_rarray_t * mr_ra_str, mr_ptrdes_t * ptrdes)
+{
+  if (ptrdes->non_persistent)
+    return (mr_ra_append_string (mr_ra_str, YAML_NULL));
+
+  switch (ptrdes->fdp->mr_type_class)
+    {
+    case MR_RECORD_TYPE_CLASS:
+    case MR_UNION_TYPE_CLASS:
+    case MR_ARRAY_TYPE_CLASS:
+      return (mr_ra_append_string (mr_ra_str, "{}"));
+    case MR_POINTER_TYPE_CLASS:
+      return (mr_ra_append_string (mr_ra_str, YAML_NULL));
+    default:
+      return (mr_ra_append_char (mr_ra_str, '0'));
+    }
+}
+
+static mr_ra_printf_t yaml_save_tbl[MR_TYPE_LAST] = {
+  [MR_TYPE_NONE] = mr_ra_printf_void,
+  [MR_TYPE_VOID] = yaml_printf_void,
+  [MR_TYPE_ENUM] = yaml_printf_bitmask,
+  [MR_TYPE_BITFIELD] = yaml_printf_bitfield,
+  [MR_TYPE_BOOL] = mr_ra_printf_bool,
+  [MR_TYPE_INT8] = mr_ra_printf_int8_t,
+  [MR_TYPE_UINT8] = mr_ra_printf_uint8_t,
+  [MR_TYPE_INT16] = mr_ra_printf_int16_t,
+  [MR_TYPE_UINT16] = mr_ra_printf_uint16_t,
+  [MR_TYPE_INT32] = mr_ra_printf_int32_t,
+  [MR_TYPE_UINT32] = mr_ra_printf_uint32_t,
+  [MR_TYPE_INT64] = mr_ra_printf_int64_t,
+  [MR_TYPE_UINT64] = mr_ra_printf_uint64_t,
+  [MR_TYPE_INT128] = mr_ra_printf_int128_t,
+  [MR_TYPE_UINT128] = mr_ra_printf_uint128_t,
+  [MR_TYPE_FLOAT] = mr_ra_printf_float,
+  [MR_TYPE_COMPLEX_FLOAT] = yaml_printf_complex_float,
+  [MR_TYPE_DOUBLE] = mr_ra_printf_double,
+  [MR_TYPE_COMPLEX_DOUBLE] = yaml_printf_complex_double,
+  [MR_TYPE_LONG_DOUBLE] = mr_ra_printf_long_double_t,
+  [MR_TYPE_COMPLEX_LONG_DOUBLE] = yaml_printf_complex_long_double_t,
+  [MR_TYPE_CHAR] = yaml_printf_char,
+  [MR_TYPE_CHAR_ARRAY] = yaml_printf_char_array,
+  [MR_TYPE_STRING] = yaml_printf_string,
+  [MR_TYPE_STRUCT] = yaml_printf_struct,
+  [MR_TYPE_FUNC] = yaml_printf_func,
+  [MR_TYPE_FUNC_TYPE] = yaml_printf_func,
+  [MR_TYPE_ARRAY] = yaml_printf_array,
+  [MR_TYPE_POINTER] = yaml_printf_array,
+  [MR_TYPE_UNION] = yaml_printf_struct,
+  [MR_TYPE_ANON_UNION] = yaml_printf_struct,
+  [MR_TYPE_NAMED_ANON_UNION] = yaml_printf_struct,
+};
+
+static mr_status_t
+yaml_pre_print_node (mr_ra_ptrdes_t * ptrs, int idx, int level, mr_rarray_t * mr_ra_str)
+{
+  mr_ra_printf_t save_handler = NULL;
+
+  if ((ptrs->ra[idx].mr_type >= 0) && (ptrs->ra[idx].mr_type < MR_TYPE_LAST))
+    save_handler = yaml_save_tbl[ptrs->ra[idx].mr_type];
+
+  if (NULL == save_handler)
+  {
+    save_handler = mr_ra_printf_void;
+    MR_MESSAGE (MR_LL_WARN, MR_MESSAGE_UNSUPPORTED_NODE_TYPE, ptrs->ra[idx].mr_type);
+  }
+
+  memset (&ptrs->ra[idx].res, 0, sizeof (ptrs->ra[idx].res));
+
+  if (mr_ra_printf (mr_ra_str, YAML_INDENT_TEMPLATE, MR_LIMIT_LEVEL (level) * YAML_INDENT_SPACES, "") < 0)
+    return (MR_FAILURE);
+
+  bool unnamed = ptrs->ra[idx].unnamed;
+  int parent = ptrs->ra[idx].parent;
+  if (unnamed && (parent >= 0))
+    if (((0 MR_FOREACH (MR_ONE_SHIFT, MR_TYPE_STRUCT, MR_TYPE_UNION, MR_TYPE_ANON_UNION, MR_TYPE_NAMED_ANON_UNION)) >> ptrs->ra[parent].mr_type) & 1)
+      unnamed = false;
+
+  if (!unnamed)
+  {
+    if (mr_ra_append_char (mr_ra_str, '-') < 0)
+      return (MR_FAILURE);
+    if (mr_ra_append_char (mr_ra_str, ' ') < 0)
+      return (MR_FAILURE);
+  }
+
+  if (ptrs->ra[idx].ref_idx >= 0)
+  {
+    if (mr_ra_printf (mr_ra_str, "*%" SCNu32, ptrs->ra[ptrs->ra[idx].ref_idx].idx) < 0)
+      return (MR_FAILURE);
+  }
+  else if (ptrs->ra[idx].flags.is_null)
+  {
+    if (mr_ra_append_string (mr_ra_str, YAML_NULL) < 0)
+      return (MR_FAILURE);
+  }
+  else if (save_handler (mr_ra_str, &ptrs->ra[idx]) < 0)
+  {
+    return (MR_FAILURE);
+  }
+
+  if (!unnamed)
+  {
+    if (mr_ra_append_string (mr_ra_str, " :") < 0)
+      return (MR_FAILURE);
+  }
+
+  return (MR_SUCCESS);
+}
+
+static mr_status_t
+yaml_post_print_node (mr_ra_ptrdes_t * ptrs, int idx, int level, mr_rarray_t * mr_ra_str)
+{
+  if (ptrs->ra[idx].res.data.string)
+    if (mr_ra_printf (mr_ra_str, YAML_INDENT_TEMPLATE, MR_LIMIT_LEVEL (level) * YAML_INDENT_SPACES, ptrs->ra[idx].res.data.string) < 0)
+      return (MR_FAILURE);
+
+  if (ptrs->ra[idx].next >= 0)
+    if (mr_ra_append_char (mr_ra_str, '\n') < 0)
+      return (MR_FAILURE);
+
+  return (MR_SUCCESS);
+}
+
+static mr_status_t
+yaml_print_node (mr_ra_ptrdes_t * ptrs, int idx, int level, mr_dfs_order_t order, void * context)
+{
+  mr_rarray_t * mr_ra_str = context;
+
+  switch (order)
+    {
+    case MR_DFS_PRE_ORDER:
+      return (yaml_pre_print_node (ptrs, idx, level, mr_ra_str));
+    case MR_DFS_POST_ORDER:
+      return (yaml_post_print_node (ptrs, idx, level, mr_ra_str));
+    default:
+      return (MR_FAILURE);
+    }
+}
+
+char *
+yaml_save (mr_ra_ptrdes_t * ptrs)
+{
+  mr_rarray_t mr_ra_str = {
+    .data = { mr_strdup ("") },
+    .MR_SIZE = sizeof (""),
+    .type = "string",
+    .alloc_size = sizeof (""),
+  };
+
+  if (NULL == mr_ra_str.data.string)
+    return (NULL);
+
+  ptrs->ptrdes_type = MR_PD_CUSTOM;
+
+  mr_ptrs_dfs (ptrs, yaml_print_node, &mr_ra_str);
+
+  return (mr_ra_str.data.string);
+}
