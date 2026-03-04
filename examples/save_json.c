8be050a9d571050330ac7cc6c1b55602457f404a
Herman Narkaytis <hnarkaytis@gmail.com>
2026-03-04 07:29:21 -0300

    Debug test failure on Windows #8

--- examples/save_json.c
+++ examples/save_json.c
@@ -18,12 +18,22 @@ main (int argc, char * argv[])
   MR_PRINT ("#1 Serialized ", strlen (mr_conf_serialized), "\n");
 
   mr_conf_t mr_conf_loaded = {};
-  //mr_status_t status = MR_LOAD_JSON (mr_conf_t, mr_conf_serialized, &mr_conf_loaded);
-  //MR_PRINT ("Load status ", (mr_status_t, &status));
+  mr_status_t status = MR_LOAD_JSON (mr_conf_t, mr_conf_serialized, &mr_conf_loaded);
+  MR_PRINT ("Load status ", (mr_status_t, &status));
 
   //mr_conf = mr_conf_loaded;
-  mr_ptrdes_t * mr_conf_serialized_ = MR_SAVE (mr_conf_t, &mr_conf_loaded);
+  mr_ptrdes_t * mr_conf_serialized_ = MR_SAVE (mr_mem_t, &mr_conf_loaded.mr_mem);
   MR_PRINT ("#2 Serialized '", (mr_ptrdes_t, mr_conf_serialized_), "'\n");
+  mr_conf_serialized_ = MR_SAVE (mr_ic_t, &mr_conf_loaded.var_types);
+  MR_PRINT ("#3 Serialized '", (mr_ptrdes_t, mr_conf_serialized_), "'\n");
+  mr_conf_serialized_ = MR_SAVE (mr_ic_t, &mr_conf_loaded.enum_by_name);
+  MR_PRINT ("#4 Serialized '", (mr_ptrdes_t, mr_conf_serialized_), "'\n");
+  mr_conf_serialized_ = MR_SAVE (mr_ic_t, &mr_conf_loaded.type_by_name);
+  MR_PRINT ("#5 Serialized '", (mr_ptrdes_t, mr_conf_serialized_), "'\n");
+  mr_conf_serialized_ = MR_SAVE (mr_ic_t, &mr_conf_loaded.field_by_name);
+  MR_PRINT ("#6 Serialized '", (mr_ptrdes_t, mr_conf_serialized_), "'\n");
+  mr_conf_serialized_ = MR_SAVE (mr_ic_t, &mr_conf_loaded.field_by_name_and_type);
+  MR_PRINT ("#7 Serialized '", (mr_ptrdes_t, mr_conf_serialized_), "'\n");
 
   mr_conf = mr_conf_saved;
   MR_FREE_RECURSIVELY (mr_conf_t, &mr_conf_loaded);
