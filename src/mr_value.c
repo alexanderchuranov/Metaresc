61eebf9804bec97e51f597beba9a6a903e39d726
Herman Narkaytis <hnarkaytis@gmail.com>
2026-03-07 14:35:53 -0800

    Debug test failure #3

--- src/mr_value.c
+++ src/mr_value.c
@@ -17,15 +17,15 @@
   This function is not static or inline intentionally - to avoid incorrect optimization.
 */
 bool
-mr_long_double_equals_double (long double x, double y)
+mr_long_double_equals_double (long double * x, double * y)
 {
-  return (x == y);
+  return (*x == (long double)*y);
 }
 
 bool
-mr_complex_long_double_equals_complex_float (complex long double x, complex float y)
+mr_complex_long_double_equals_complex_float (complex long double * x, complex float * y)
 {
-  return (x == y);
+  return (*x == (complex long double)*y);
 }
 
 mr_status_t
@@ -74,34 +74,39 @@ mr_value_to_mr_ptrdes (mr_ptrdes_t * ptrdes, mr_value_t * mr_value, char * str)
       break;
 
     case MR_VT_LONG_DOUBLE:
-      if (mr_long_double_equals_double (mr_value->vt_long_double, mr_value->vt_long_double))
-	{
-	  fprintf (stderr, "Long double %.20Lg converged to double %.20g\n", mr_value->vt_long_double, (double)mr_value->vt_long_double);
-	  ptrdes->vt_double = mr_value->vt_long_double;
-	  ptrdes->value_type = MR_VT_DOUBLE;
-	}
-      else
-	{
-	  ptrdes->vt_long_double = MR_CALLOC (1, sizeof (*ptrdes->vt_long_double));
-	  if (NULL == ptrdes->vt_long_double)
-	    return (MR_FAILURE);
-	  *ptrdes->vt_long_double = mr_value->vt_long_double;
-	}
+      {
+	double _double = mr_value->vt_long_double;
+	if (mr_long_double_equals_double (&mr_value->vt_long_double, &_double))
+	  {
+	    ptrdes->vt_double = _double;
+	    ptrdes->value_type = MR_VT_DOUBLE;
+	  }
+	else
+	  {
+	    ptrdes->vt_long_double = MR_CALLOC (1, sizeof (*ptrdes->vt_long_double));
+	    if (NULL == ptrdes->vt_long_double)
+	      return (MR_FAILURE);
+	    *ptrdes->vt_long_double = mr_value->vt_long_double;
+	  }
+      }
       break;
 
     case MR_VT_COMPLEX_LONG_DOUBLE:
-      if (mr_complex_long_double_equals_complex_float (mr_value->vt_complex_long_double, mr_value->vt_complex_long_double))
-	{
-	  ptrdes->vt_complex_float = mr_value->vt_complex_long_double;
-	  ptrdes->value_type = MR_VT_COMPLEX_FLOAT;
-	}
-      else
-	{
-	  ptrdes->vt_complex_long_double = MR_CALLOC (1, sizeof (*ptrdes->vt_complex_long_double));
-	  if (NULL == ptrdes->vt_complex_long_double)
-	    return (MR_FAILURE);
-	  *ptrdes->vt_complex_long_double = mr_value->vt_complex_long_double;
-	}
+      {
+	complex float _complex_float = mr_value->vt_complex_long_double;
+	if (mr_complex_long_double_equals_complex_float (&mr_value->vt_complex_long_double, &_complex_float))
+	  {
+	    ptrdes->vt_complex_float = _complex_float;
+	    ptrdes->value_type = MR_VT_COMPLEX_FLOAT;
+	  }
+	else
+	  {
+	    ptrdes->vt_complex_long_double = MR_CALLOC (1, sizeof (*ptrdes->vt_complex_long_double));
+	    if (NULL == ptrdes->vt_complex_long_double)
+	      return (MR_FAILURE);
+	    *ptrdes->vt_complex_long_double = mr_value->vt_complex_long_double;
+	  }
+      }
       break;
 
     default:
